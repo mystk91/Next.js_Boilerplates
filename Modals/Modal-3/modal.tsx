d5e04057d510bae932223ebd15d7b30eb46d229d
mystk91 <90066188+mystk91@users.noreply.github.com>
2024-10-09 20:26:02 -0400

    New Modal
    made a better version of Modal2, mostly the same

--- Modals/Modal-3/modal.tsx
+++ Modals/Modal-3/modal.tsx
@@ -50,26 +50,24 @@ export default function Modal({ children, closeFunction }: ModalProps) {
   return (
     <FocusTrap>
       <div className={styles.backdrop} onClick={closeModal}>
-        <div className={styles.backdropTop}>
-          <button
-            className={styles.closeButton}
-            onClick={closeModal}
-            aria-label={"Close Modal"}
+        <button
+          className={styles.closeButton}
+          onClick={closeModal}
+          aria-label={"Close Modal"}
+        >
+          <svg
+            id="close-icon"
+            width="122.878px"
+            height="122.88px"
+            viewBox="0 0 122.878 122.88"
+            enable-background="new 0 0 122.878 122.88"
+            className={styles.closeIcon}
           >
-            <svg
-              id="close-icon"
-              width="122.878px"
-              height="122.88px"
-              viewBox="0 0 122.878 122.88"
-              enable-background="new 0 0 122.878 122.88"
-              className={styles.closeIcon}
-            >
-              <g>
-                <path d="M1.426,8.313c-1.901-1.901-1.901-4.984,0-6.886c1.901-1.902,4.984-1.902,6.886,0l53.127,53.127l53.127-53.127 c1.901-1.902,4.984-1.902,6.887,0c1.901,1.901,1.901,4.985,0,6.886L68.324,61.439l53.128,53.128c1.901,1.901,1.901,4.984,0,6.886 c-1.902,1.902-4.985,1.902-6.887,0L61.438,68.326L8.312,121.453c-1.901,1.902-4.984,1.902-6.886,0 c-1.901-1.901-1.901-4.984,0-6.886l53.127-53.128L1.426,8.313L1.426,8.313z" />
-              </g>
-            </svg>
-          </button>
-        </div>
+            <g>
+              <path d="M1.426,8.313c-1.901-1.901-1.901-4.984,0-6.886c1.901-1.902,4.984-1.902,6.886,0l53.127,53.127l53.127-53.127 c1.901-1.902,4.984-1.902,6.887,0c1.901,1.901,1.901,4.985,0,6.886L68.324,61.439l53.128,53.128c1.901,1.901,1.901,4.984,0,6.886 c-1.902,1.902-4.985,1.902-6.887,0L61.438,68.326L8.312,121.453c-1.901,1.902-4.984,1.902-6.886,0 c-1.901-1.901-1.901-4.984,0-6.886l53.127-53.128L1.426,8.313L1.426,8.313z" />
+            </g>
+          </svg>
+        </button>
         <div className={styles.modal} role="dialog" aria-modal="true">
           {children}
         </div>
