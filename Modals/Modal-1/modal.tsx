fde89576cbd71000fb80ca85cbbc7aa6358ff8b3
mystk91 <90066188+mystk91@users.noreply.github.com>
2025-05-04 23:29:25 -0400

    Modal Update
    Created a Versatile Modal component and slightly updated my old modals.

--- Modals/Modal-1/modal.tsx
+++ Modals/Modal-1/modal.tsx
@@ -8,7 +8,7 @@ import React, {
   useContext,
 } from "react";
 import styles from "./modal.module.css";
-import FocusTrap from "focus-trap-react";
+import { FocusTrap } from "focus-trap-react";
 
 /* A flexible modal component that displays inputed content
  * children - the content that will be on the modal
@@ -49,7 +49,7 @@ export default function Modal({ children, closeFunction }: ModalProps) {
 
   return (
     <FocusTrap>
-      <div className={styles.backdrop} onClick={closeModal}>
+      <div className={styles.backdrop} /* onClick={closeModal} */>
         <div className={styles.modal} role="dialog" aria-modal="true">
           <button
             className={styles.closeButton}
@@ -58,15 +58,10 @@ export default function Modal({ children, closeFunction }: ModalProps) {
           >
             <svg
               id="close-icon"
-              width="122.878px"
-              height="122.88px"
               viewBox="0 0 122.878 122.88"
-              enable-background="new 0 0 122.878 122.88"
               className={styles.closeIcon}
             >
-              <g>
-                <path d="M1.426,8.313c-1.901-1.901-1.901-4.984,0-6.886c1.901-1.902,4.984-1.902,6.886,0l53.127,53.127l53.127-53.127 c1.901-1.902,4.984-1.902,6.887,0c1.901,1.901,1.901,4.985,0,6.886L68.324,61.439l53.128,53.128c1.901,1.901,1.901,4.984,0,6.886 c-1.902,1.902-4.985,1.902-6.887,0L61.438,68.326L8.312,121.453c-1.901,1.902-4.984,1.902-6.886,0 c-1.901-1.901-1.901-4.984,0-6.886l53.127-53.128L1.426,8.313L1.426,8.313z" />
-              </g>
+              <path d="M1.426,8.313c-1.901-1.901-1.901-4.984,0-6.886c1.901-1.902,4.984-1.902,6.886,0l53.127,53.127l53.127-53.127 c1.901-1.902,4.984-1.902,6.887,0c1.901,1.901,1.901,4.985,0,6.886L68.324,61.439l53.128,53.128c1.901,1.901,1.901,4.984,0,6.886 c-1.902,1.902-4.985,1.902-6.887,0L61.438,68.326L8.312,121.453c-1.901,1.902-4.984,1.902-6.886,0 c-1.901-1.901-1.901-4.984,0-6.886l53.127-53.128L1.426,8.313L1.426,8.313z" />
             </svg>
           </button>
           {children}
